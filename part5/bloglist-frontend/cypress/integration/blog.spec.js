c470971402b718a3cc0440e582af1688b66fac4a
Viet Le <lttviet@gmail.com>
2021-11-04 15:04:06 -0400

    Add cypress lib

---
+++ part5/bloglist-frontend/cypress/integration/blog.spec.js
@@ -0,0 +1,12 @@
+/* eslint-disable func-names */
+/* eslint-disable prefer-arrow-callback */
+describe('Blog app', function () {
+  beforeEach(function () {
+    cy.request('POST', 'http://localhost:3001/api/tests/reset')
+    cy.visit('http://localhost:3000')
+  })
+
+  it('Login form is shown', function () {
+    cy.contains('login')
+  })
+})
