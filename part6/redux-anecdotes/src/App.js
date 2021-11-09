24180d24e54feee235f1a6a867bd18840fc24637
Viet Le <lttviet@gmail.com>
2021-11-09 12:14:49 -0500

    Finish 6.8

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,35 +1,12 @@
-import React, { useMemo } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import React from 'react'
 import AnecdoteForm from './components/AnecdoteForm'
-import { voteAnecdote } from './reducers/anecdoteReducer'
+import AnecdoteList from './components/AnecdoteList'
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
-  const dispatch = useDispatch()
-
-  const orderedAnecdotes = useMemo(() => {
-    return [...anecdotes].sort((a, b) => a.votes > b.votes)
-  }, [anecdotes])
-
-  const vote = (id) => {
-    dispatch(voteAnecdote(id))
-  }
-
   return (
     <div>
       <h2>Anecdotes</h2>
-      {orderedAnecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
-      <h2>create new</h2>
+      <AnecdoteList />
       <AnecdoteForm />
     </div>
   )
---
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -0,0 +1,34 @@
+import React, { useMemo } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { voteAnecdote } from '../reducers/anecdoteReducer'
+
+const AnecdoteList = () => {
+  const anecdotes = useSelector(state => state)
+  const dispatch = useDispatch()
+
+  const orderedAnecdotes = useMemo(() => {
+    return [...anecdotes].sort((a, b) => a.votes > b.votes)
+  }, [anecdotes])
+
+  const vote = (id) => {
+    dispatch(voteAnecdote(id))
+  }
+
+  return (
+    <>
+      {orderedAnecdotes.map((anecdote) =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      )}
+    </>
+  )
+}
+
+export default AnecdoteList
