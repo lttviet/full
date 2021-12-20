97449109cf3e321a1ba6ffbf2fe6f73bca139540
Viet Le <lttviet@gmail.com>
2021-12-20 13:47:21 -0500

    Add GET /users/:id

--- part4/blog/controllers/users.js
+++ part4/blog/controllers/users.js
@@ -11,6 +11,13 @@ usersRouter.get('/', async (request, response) => {
   response.json(users)
 })
 
+usersRouter.get('/:id', async (request, response) => {
+  const user = await User
+    .findById(request.params.id)
+    .populate('blogs', { title: 1 })
+  response.json(user)
+})
+
 usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body
 
